bc84a0b1bd39a01cd9d005e326d9734da0d74563
Matt Aitken <matt@mattaitken.com>
2022-09-23 18:40:52 +0100

    With code comments

--- components/RepoList.tsx
+++ components/RepoList.tsx
@@ -1,11 +1,19 @@
-/* eslint-disable @next/next/no-img-element */
-import { repos } from "@apihero/github";
+// 1. This function is used to create a React Hook for an API endpoint
 import { createEndpoint } from "@apihero/react";
+// 2. "repos" is a group of endpoints. We're going to use "listForUser" from it
+import { repos } from "@apihero/github";
 import styles from "../styles/Home.module.css";
 
+// 3. We create a React Hook for the "listForUser" endpoint,
+//    outside of the component so it's only created once
 const useRepoList = createEndpoint(repos.listForUser);
 
 export function RepoList({ username }: { username: string }) {
+  // 4. The hook has type-safe inputs and outputs
+  //    "data" is the response on success, with the correct type
+  //    "status" is the status of the request: "loading", "error" or "success"
+  //    "error" is the error object if there is an error
+  //    TypeScript will force you to pass in the correct inputs, username in this case is required
   const { data, status, error } = useRepoList({
     username,
     type: "owner",
@@ -14,6 +22,7 @@ export function RepoList({ username }: { username: string }) {
 
   return (
     <>
+      {/* 5. We can use the status to show a loading indicator, an error message, or the data */}
       {status === "loading" ? (
         <img
           src="https://media.giphy.com/media/VseXvvxwowwCc/giphy.gif"
@@ -30,11 +39,13 @@ export function RepoList({ username }: { username: string }) {
         </div>
       ) : (
         <div className={styles.grid}>
+          {/* 6. The data is an array of repositories, we sort them so the most popular ones are first */}
           {data
             .sort(
               (a, b) => (b.stargazers_count ?? 0) - (a.stargazers_count ?? 0)
             )
             .map((item) => (
+              // 7. Each repository is a card with a link to the repository and some basic info
               <a
                 key={item.id}
                 href={item.html_url}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,25 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useState } from "react";
-import { RepoList } from "../components/RepoList";
 import styles from "../styles/Home.module.css";
+// 1. We will create this file in the next step
+import { RepoList } from "../components/RepoList";
 
 const Home: NextPage = () => {
+  // 2. This state will be used for the input field, and to perform the API call
   const [username, setUsername] = useState<string | undefined>(undefined);
 
   return (
     <div className={styles.container}>
       <Head>
         <title>User’s repositories</title>
-        <meta name="description" content="Quickly search GitHub" />
+        <meta name="description" content="User's most popular repositories" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <header className={styles.header}>
+          {/* 3. We use the username state in the normal React way */}
           <input
             type="text"
             value={username}
@@ -26,6 +29,7 @@ const Home: NextPage = () => {
           />
         </header>
 
+        {/* 4. The RepoList component is where the magic happens */}
         {username && <RepoList username={username} />}
       </main>
     </div>
